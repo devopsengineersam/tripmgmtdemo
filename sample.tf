7e66280b7a5dbf8fced2bafbe809b9e2dba4d6c6
devopsengineersam-pro <ogemdiisiguzo@gmail.com>
2022-08-29 15:58:52 -0500

    terraform

--- sample.tf
+++ sample.tf
@@ -1,3 +1,11 @@
 terraform {
-  
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        versionversion = "4.27.0"
+    }
+  }
+  provider_meta "aws" {
+    region = "us-east-1"
+  }
 }
